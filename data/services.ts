596d7c026d7d3eec890b567980ca499390c89890
Nelson Malgas <zmalgas69@gmail.com>
2024-08-02 10:32:51 +0200

    Modified services data

--- data/services.ts
+++ data/services.ts
@@ -2,7 +2,7 @@ interface Service {
     num: string;
     title: string;
     description: string;
-    href: string
+    href: string;
 }
 
 const services: Service[] = [
@@ -10,31 +10,37 @@ const services: Service[] = [
         num: "01",
         title: "Web Developer",
         description: "Designs and builds websites, ensuring they are functional, user-friendly, and visually appealing.",
-        href:""
+        href: ""
     },
     {
         num: "02",
-        title: "Graphic Designer",
-        description: "Creates visual content to communicate messages through digital and print media, including logos, brochures, and advertisements.",
-        href:""
+        title: "Fullstack Developer",
+        description: "Handles both the front-end and back-end development of web applications, ensuring seamless integration and performance.",
+        href: ""
     },
     {
         num: "03",
         title: "SEO Specialist",
         description: "Optimizes websites to improve their search engine rankings, driving more organic traffic and enhancing online visibility.",
-        href:""
+        href: ""
     },
     {
         num: "04",
-        title: "Content Writer",
-        description: "Produces engaging and relevant written content for websites, blogs, social media, and other digital platforms.",
-        href:""
+        title: "Progressive Web Apps",
+        description: "Develops web applications that load like regular web pages but offer user functionality such as working offline, push notifications, and device hardware access.",
+        href: ""
     },
     {
         num: "05",
-        title: "Digital Marketing Expert",
-        description: "Develops and implements marketing strategies to promote products or services online, utilizing various digital channels.",
-        href:""
+        title: "Database Integration",
+        description: "Integrates databases into web applications to store, manage, and retrieve data efficiently, ensuring data integrity and security.",
+        href: ""
+    },
+    {
+        num: "06",
+        title: "Mobile App Developer",
+        description: "Creates applications for mobile devices, ensuring they are functional, user-friendly, and optimized for performance across various mobile platforms.",
+        href: ""
     }
 ]
 
