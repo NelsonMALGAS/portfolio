b15cc5017cb1c509a68cbbca6d6f70a17c77d08e
Nelson Malgas <zmalgas69@gmail.com>
2025-01-24 14:11:40 +0200

    Fixed stats data

--- data/stats.ts
+++ data/stats.ts
@@ -2,7 +2,7 @@ const stats = [
     { num: 2, text: "Years of experience" },
     { num: 52, text: "Projects completed" },
     { num: 10, text: "Technologies Mastered" },
-    { num: 750, text: "Code Commits" },
+    { num: 907, text: "Code Commits" },
 ]
 
 export default stats
