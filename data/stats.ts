20fd2719b56f4d7f00fbf9599621f39b824c35ef
Nelson Zongezile Malgas <zmalgas69@gmail.com>
2026-01-13 14:55:25 +0200

    Update stats.ts

--- data/stats.ts
+++ data/stats.ts
@@ -1,5 +1,5 @@
 const stats = [
-    { num: 2, text: "Years of experience" },
+    { num: 3, text: "Years of experience" },
     { num: 52, text: "Projects completed" },
     { num: 10, text: "Technologies Mastered" },
     { num: 907, text: "Code Commits" },
